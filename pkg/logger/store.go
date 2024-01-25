d21cdb2ada2672622251518bc22b5bf0d6d2805b
BryceDouglasJames <brycejames1214@yahoo.com>
2024-01-25 02:04:28 -0500

    Forgot to change test from before nil store check

--- pkg/logger/store.go
+++ pkg/logger/store.go
@@ -46,7 +46,7 @@ func WithBufferSize(size uint64) StoreOptions {
 // Creates a new store with the given options.
 // It initializes a store with a buffer of the specified size and associates it with the provided file, if any.
 // The function applies a series of StoreOptions functions to configure the store.
-func NewStore(f *os.File, optFns ...StoreOptions) (*store, error) {
+func NewStore(f *os.File, optFns ...StoreOptions) (filestore *store, err error) {
 	// Set options
 	opts := defaultOptions()
 	for _, fn := range optFns {
@@ -54,7 +54,7 @@ func NewStore(f *os.File, optFns ...StoreOptions) (*store, error) {
 	}
 
 	// Create new store instance
-	new_store := &store{}
+	newStore := &store{}
 
 	if opts.File != nil {
 		// Verify file from options
@@ -62,18 +62,18 @@ func NewStore(f *os.File, optFns ...StoreOptions) (*store, error) {
 		if err != nil {
 			return nil, err
 		}
-		new_store.File = opts.File
+		newStore.File = opts.File
 
 		// Add mutex to store
-		new_store.mu = sync.Mutex{}
+		newStore.mu = sync.Mutex{}
 
 		// Buffer to fit file size
-		file_size := uint64(file.Size())
-		buf := bufio.NewWriterSize(opts.File, int(file_size))
-		new_store.buf = *buf
+		fileSize := uint64(file.Size())
+		buf := bufio.NewWriterSize(opts.File, int(fileSize))
+		newStore.buf = *buf
 
 		// Return store with passed reference pointer
-		return new_store, nil
+		return newStore, nil
 	}
 
 	// Use the buffer size specified in options for the buffered writer.
@@ -82,9 +82,11 @@ func NewStore(f *os.File, optFns ...StoreOptions) (*store, error) {
 	// For instance, setting it to the size of a standard page can optimize for page-aligned I/O.
 	bufSize := opts.BufferSize
 	buf := bufio.NewWriterSize(f, int(bufSize))
-	new_store.buf = *buf
-	new_store.mu = sync.Mutex{}
+	newStore.buf = *buf
+	newStore.mu = sync.Mutex{}
 
-	return new_store, nil
+	return newStore, nil
 
 }
+
+func (s *store) Append(page []byte) (size uint64, pos uint64, err error) { return 0, 0, nil }
--- pkg/logger/store_test.go
+++ pkg/logger/store_test.go
@@ -23,9 +23,31 @@ func TestNewStoreWithValidFile(t *testing.T) {
 }
 
 func TestNewStoreWithNilFile(t *testing.T) {
-	// Test creating a new store with nil file
-	_, err := NewStore(nil)
-	if err == nil {
-		t.Errorf("Expected error when creating store with nil file, got none")
+	// Create a new store with nil file
+	store, err := NewStore(nil)
+	if err != nil {
+		t.Errorf("Expected no error when creating store with nil file, got: %v", err)
+	}
+
+	// Verify that the file in the store is nil initially
+	if store.File != nil {
+		t.Errorf("Expected nil file in store, got: %v", store.File)
+	}
+
+	// Create a temporary file
+	tmpfile, err := os.CreateTemp("", "*.log")
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+
+	// Clean up
+	defer os.Remove(tmpfile.Name())
+
+	// Assign file
+	store.File = tmpfile
+
+	// Verify that the file in the store is correctly assigned
+	if store.File != tmpfile {
+		t.Errorf("Expected file in store to be %v, got: %v", tmpfile, store.File)
 	}
 }
