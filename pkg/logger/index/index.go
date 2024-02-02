ec603bccfd42459bb876e68b555a84510fdb1844
BryceDouglasJames <brycejames1214@yahoo.com>
2024-02-01 23:24:03 -0500

    Add autocreate index option, added close func

--- pkg/logger/index/index.go
+++ pkg/logger/index/index.go
@@ -1,6 +1,7 @@
 package index
 
 import (
+	"errors"
 	"os"
 
 	"github.com/tysonmote/gommap"
@@ -16,6 +17,7 @@ type Options struct {
 	File             *os.File
 	FilePath         string
 	UseMemoryMapping bool
+	AutoCreate       bool
 }
 
 // Represents a function that applies configuration options to an Options instance
@@ -34,6 +36,7 @@ func DefaultOptions() *Options {
 		File:             nil,             // nil pointer
 		FilePath:         "./default.txt", // destination of temp generate
 		UseMemoryMapping: false,
+		AutoCreate:       true,
 	}
 }
 
@@ -48,6 +51,13 @@ func WithFile(f *os.File) IndexOptions {
 func WithFilePath(path string) IndexOptions {
 	return func(opts *Options) {
 		opts.FilePath = path
+	}
+}
+
+// Option to enable or disable automatic index file creation.
+func WithAutoCreate(autoCreate bool) IndexOptions {
+	return func(opts *Options) {
+		opts.AutoCreate = autoCreate
 	}
 }
 
@@ -72,10 +82,20 @@ func NewIndex(optFns ...IndexOptions) (*Index, error) {
 
 	// Check if a custom file is provided in options
 	if opts.File == nil {
-		// Open the default file, create if it does not exist, and set it to append mode
-		newIndex.file, err = os.OpenFile(opts.FilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return nil, err // Return an error if the file cannot be opened or created
+		// We want to be careful if we decide to auto create the index files for data integrity and consistency sake.
+		// So we will let that be an option.
+		if opts.AutoCreate {
+			// Attempt to open or create the file only if AutoCreate is true.
+			newIndex.file, err = os.OpenFile(opts.FilePath, os.O_RDWR|os.O_CREATE, 0666)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			// Attempt to open the file without creating it.
+			newIndex.file, err = os.Open(opts.FilePath)
+			if err != nil {
+				return nil, err
+			}
 		}
 	} else if opts.File != nil {
 		// If the file is already open, check if it's usable
@@ -103,16 +123,44 @@ func NewIndex(optFns ...IndexOptions) (*Index, error) {
 
 	// TODO: Add Segment Management
 
-	// Memory-map the file if requested
+	// Attempt to memory-map the file if requested
 	if opts.UseMemoryMapping {
-		newIndex.mmap, err = gommap.Map(newIndex.file.Fd(), gommap.PROT_READ|gommap.PROT_WRITE, gommap.MAP_SHARED)
+		// Ensure the file descriptor supports the intended memory map protections.
+		mmapProt := gommap.PROT_READ | gommap.PROT_WRITE
+		mmapFlags := gommap.MAP_SHARED
+
+		newIndex.mmap, err = gommap.Map(newIndex.file.Fd(), mmapProt, mmapFlags)
 		if err != nil {
 			return nil, err
 		}
 		newIndex.UseMemoryMapping = true
-	} else {
-		newIndex.UseMemoryMapping = false
 	}
 
 	return newIndex, nil
+}
+
+func (i *Index) Close() error {
+	// Check if mmap exists and is valid before attempting to sync
+	if i.mmap != nil {
+		if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+			return err
+		}
+	} else if i.UseMemoryMapping {
+		return errors.New("something is very wrong index mmap should not be nil")
+	}
+
+	// Ensure file is synced and truncated properly
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+
+	// Close the file at the end
+	if err := i.file.Close(); err != nil {
+		return err
+	}
+
+	return nil
 }
--- pkg/logger/index/index_test.go
+++ pkg/logger/index/index_test.go
@@ -23,3 +23,49 @@ func TestNewIndexDefaultOptions(t *testing.T) {
 		t.Error("Expected memory mapping to be disabled by default")
 	}
 }
+
+func TestNewIndexWithMemoryMapping(t *testing.T) {
+	// Create a temporary file for testing
+	tmpFile, err := os.CreateTemp("", "example_*.log")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+
+	// Close the file as NewStore will open it
+	tmpFile.Close()
+
+	// Create new index with memory mapped option
+	idx, err := NewIndex(WithMemoryMapping(true), WithFilePath(tmpFile.Name()))
+	if err != nil {
+		t.Fatalf("Failed to create index with memory mapping enabled: %v", err)
+	}
+
+	// Clean up
+	defer os.Remove(tmpFile.Name())
+
+	if !idx.UseMemoryMapping {
+		t.Error("Expected memory mapping to be enabled")
+	}
+}
+
+func TestIndexClose(t *testing.T) {
+	// Create a temporary file for testing
+	tmpFile, err := os.CreateTemp("", "index_test")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %v", err)
+	}
+
+	// Clean up
+	defer os.Remove(tmpFile.Name())
+
+	// Initialize the Index with the temporary file
+	i, err := NewIndex(WithFile(tmpFile), WithMemoryMapping(true))
+	if err != nil {
+		t.Fatalf("Failed to create Index: %v", err)
+	}
+
+	// Close the Index and ensure no errors are returned
+	if err := i.Close(); err != nil {
+		t.Errorf("Failed to close Index: %v", err)
+	}
+}
